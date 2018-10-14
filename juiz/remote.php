7291a4c780d4af8e0918c9fc5d39367aee38ffbf
Tiago Martines <titimartines@gmail.com>
2018-10-14 19:09:20 -0300

    Add CORS support to remote.php.

--- juiz/remote.php
+++ juiz/remote.php
@@ -7,6 +7,8 @@ use Baja\Model\InputQuery;
 use Baja\Model\ProvaQuery;
 use Propel\Runtime\Exception\PropelException;
 
+cors();
+
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE);
 
@@ -30,7 +32,7 @@ if (array_key_exists("truncate", $input) && $input["truncate"] == "all") {
 }
 
 if (array_key_exists("raceTime", $input)) {
-    $prova->setTempo(time() - $input["raceTime"]);
+    $prova->setTempo(time() - $input["raceTime"] / 1000);
     $prova->save();
 }
 
@@ -53,3 +55,27 @@ if (array_key_exists("cars", $input)) {
 }
 
 die("NO ACTION");
+
+function cors() {
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+}
