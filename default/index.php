4e32187fe3d982dccdb5fa9ae28c71aa8aa7decf
Tiago Martines <titimartines@gmail.com>
2020-08-15 14:50:29 -0300

    Add default/hub placeholder

---
+++ default/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Baja SAE BRASIL Online</title>
+    <meta name="viewport" content="width=device-width"/>
+    <meta charset="UTF-8"/>
+    <link class="theme" rel="stylesheet" href="css/theme.blue.css">
+    <link class="theme" rel="stylesheet" href="css/slider.css">
+    <link rel="icon" href="img/baja.png" type="image/png">
+</head>
+<body class="body-blue">
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-92791788-1', 'auto');
+    ga('send', 'pageview');
+</script>
+
+<div style="font-size: 30px;">
+    <br/>
+    <br/>
+    <b>Bem-Vindo ao Hub BajaSaeBrasil.Online</b>
+    <br/><br/>
+    Escolha um site:
+    <br/>
+    <br/>
+    <b>Resultados</b><br/>
+    <a href="https://resultados.bajasaebrasil.online/">https://resultados.bajasaebrasil.online/</a>
+    <br/>
+    <br/>
+    <b>Fórum</b><br/>
+    <a href="https://forum.bajasaebrasil.online/">https://forum.bajasaebrasil.online/</a>
+    <br/>
+    <br/>
+    <b>Certificados</b><br/>
+    <a href="https://certificado.bajasaebrasil.online/">https://certificado.bajasaebrasil.online/</a>
+</div>
+
+</body>
+</html>
