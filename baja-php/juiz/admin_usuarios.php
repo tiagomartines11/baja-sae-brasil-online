668341e95cb6b2d0c4ae6577707b6475cda604c9
jbresolin <joaobresolin@gmail.com>
2026-07-20 00:26:57 -0300

    Added missing add user option.

--- baja-php/juiz/admin_usuarios.php
+++ baja-php/juiz/admin_usuarios.php
@@ -1,10 +1,28 @@
 <?php
 namespace Baja\Juiz;
 
+use Baja\Model\User;
 use Baja\Model\UserQuery;
 use Baja\Session;
 
 Session::permissionCheck("admin");
+
+if (@$_REQUEST['act'] == 'create') {
+    $username = trim($_POST['username'] ?? '');
+    if ($username !== '') {
+        $user = UserQuery::create()->findOneByUsername($username);
+        if (!$user) {
+            // Persist with just the sentinel so the row exists; the editor is where
+            // real permissions get added.
+            $user = new User();
+            $user->setUsername($username);
+            $user->setPermissions(['index']);
+            $user->save();
+        }
+        header("Location: admin_usuario.php?id=".$user->getUserId()); exit;
+    }
+    header("Location: admin_usuarios.php"); exit;
+}
 
 $users = UserQuery::create()->orderByUsername()->find();
 
@@ -36,13 +54,22 @@ Template::printHeader("Usuários", false, false);
         <tbody>
 <?php foreach ($users as $u) { ?>
             <tr>
-                <td class="username"><?= htmlspecialchars($u->getUsername()) ?></td>
+                <td class="username"><?= htmlspecialchars($u->getUsername() ?? '') ?></td>
                 <td style="text-align: center;"><?= count(array_diff($u->getPermissions(), \Baja\Model\User::SENTINEL_PERMISSIONS)) ?></td>
                 <td style="text-align: center;"><a href="admin_usuario.php?id=<?= $u->getUserId() ?>"><span>&nbsp;✏️&nbsp;</span></a></td>
             </tr>
 <?php } ?>
         </tbody>
         <tfoot>
+            <form action="admin_usuarios.php?act=create" method="POST">
+            <tr>
+                <th colspan="3" style="height: 30px;">
+                    Novo usuário:
+                    <input type="text" name="username" id="new_username" autocomplete="off" style="padding: 4px; width: 45%;" />
+                    <input type="submit" id="add_user" value="Adicionar usuário" disabled />
+                </th>
+            </tr>
+            </form>
             <tr>
                 <th colspan="3" style="height: 30px">
                     <input type="button" onclick="location.href='admin_fila_acesso.php';" value="Acesso em massa às filas" />
@@ -60,6 +87,9 @@ Template::printHeader("Usuários", false, false);
             var name = $(this).find('.username').text().toLowerCase();
             $(this).toggle(name.indexOf(q) !== -1);
         });
+    });
+    $('#new_username').on('input', function () {
+        $('#add_user').prop('disabled', $.trim($(this).val()) === '');
     });
 </script>
 
